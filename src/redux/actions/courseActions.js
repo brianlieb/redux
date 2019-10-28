04a3cbb244e566af6ce3b4b6b97dc011b2d1949f
Brian Lieb <brianlieb@hotmail.com>
2019-10-27 21:09:10 -0800

    Chapter 11 over

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,19 +1,35 @@
 import * as types from "./actionTypes"
 import * as courseApi from "../../api/courseApi"
 
-
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course };
-}
-
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function updateCourseSuccess(courses) {
+    return { type: types.UPDATE_COURSE_SUCCESS, courses};
+}
+
+export function createCourseSuccess(courses) {
+    return { type: types.CREATE_COURSE_SUCCESS, courses};
 }
 
 export function loadCourses() {
     return function (dispatch) {
         return courseApi.getCourses()
             .then(courses => { dispatch(loadCoursesSuccess(courses))})
+            .catch(error => {throw error;})
+    }
+}
+
+export function saveCourse(course) {
+    return function (dispatch) {
+        return courseApi.saveCourse(course)
+            .then(
+                savedCourse => {
+                    course.id
+                    ? dispatch(updateCourseSuccess(savedCourse))
+                    : dispatch(createCourseSuccess(savedCourse));
+                })
             .catch(error => {throw error;})
     }
 }
